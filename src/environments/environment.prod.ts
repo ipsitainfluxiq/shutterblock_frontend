541b3a2d618c2e1a6e248866b94e165a1145ccf1
ipsitainfluxiq <ipsita.influxiq@gmail.com>
2019-04-02 12:50:06 +0530

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
-  API_URL: 'http://18.222.26.198:7002/',
+  API_URL: 'http://18.222.26.198:7001/',
+  Model_Image_Url: 'http://18.222.26.198/upload/modelimages/',
+  Brand_Image_Url: 'http://18.222.26.198/upload/brandimages/',
+  brandfolder: 'brandimages',
+  modelfolder: 'modelimages',
   environment: 'production'
 }
