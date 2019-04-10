eaebc8ac1acd4ad9f2887073295f30e35bc1e47e
ipsitainfluxiq <ipsita.influxiq@gmail.com>
2019-04-10 19:41:06 +0530

    by ips

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -7,8 +7,8 @@ export const environment = {
     Model_Image_Url: 'https://nodessl.influxiq.com/upload/modelimages/',
     // Brand_Image_Url: 'http://18.222.26.198/upload/brandimages/',
     Brand_Image_Url: 'https://nodessl.influxiq.com/upload/brandimages/',*/
-    Brand_Image_Url: 'https://development.shatterblok.com/upload/brandimages',
-    Model_Image_Url: 'https://development.shatterblok.com/upload/modelimages',
+    Brand_Image_Url: 'https://shatterblok.com/upload/brandimages',
+    Model_Image_Url: 'https://shatterblok.com/upload/modelimages',
     loginurl: 'http://developmentbackoffice.shatterblok.com/',
     brandfolder: 'brandimages',
     modelfolder: 'modelimages',
