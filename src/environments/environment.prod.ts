b7ddd352a925404252393ddad176917086e1620c
ipsitainfluxiq <ipsita.influxiq@gmail.com>
2019-04-10 18:55:19 +0530

    ips

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,11 @@ export const environment = {
   production: true,
   // API_URL: 'http://18.222.26.198:7002/',
   API_URL: 'https://nodessl.influxiq.com:6033',
-  Model_Image_Url: 'https://nodessl.influxiq.com/upload/modelimages/',
-  Brand_Image_Url: 'https://nodessl.influxiq.com/upload/brandimages/',
-    loginurl: 'http://developmentbackoffice.shatterblok.com/',
+  // Model_Image_Url: 'https://nodessl.influxiq.com/upload/modelimages',
+  /* Brand_Image_Url: 'https://nodessl.influxiq.com/upload/brandimages',*/
+  Brand_Image_Url: 'https://development.shatterblok.com/upload/brandimages',
+  Model_Image_Url: 'https://development.shatterblok.com/upload/modelimages',
+  loginurl: 'http://developmentbackoffice.shatterblok.com/',
   brandfolder: 'brandimages',
   modelfolder: 'modelimages',
   environment: 'production'
